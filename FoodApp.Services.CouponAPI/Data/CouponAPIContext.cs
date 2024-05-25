fc9a4467768b99311c32c7e45e01371834d8eef1
Samil Quezada Suriel <20222170@itla.edu.do>
2024-05-24 23:17:54 -0400

    Add project files.

---
+++ FoodApp.Services.CouponAPI/Data/CouponAPIContext.cs
@@ -0,0 +1,14 @@
+﻿using FoodApp.Services.CouponAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FoodApp.Services.CouponAPI.Data
+{
+    public class CouponAPIContext : DbContext
+    {
+        public CouponAPIContext(DbContextOptions<CouponAPIContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Coupon> Coupons { get; set; }
+    }
+}
